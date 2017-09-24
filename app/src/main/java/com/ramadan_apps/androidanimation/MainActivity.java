ca9eacdf78128670bef5dfc354d6b94a061aee68
elsamet <eng.mahmoudramadan2012@gmail.com>
2017-09-24 09:44:49 +0200

    project setup

--- app/src/main/java/com/ramadan_apps/androidanimation/MainActivity.java
+++ app/src/main/java/com/ramadan_apps/androidanimation/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ramadan_apps.androidanimation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void imageClicked(View thumbnailView) {
+        ImageView thumbnail = (ImageView)thumbnailView;
+        ImageView animatedImage = (ImageView) findViewById(R.id.animatedImage);
+
+        animatedImage.setImageDrawable(thumbnail.getDrawable());
+        animatedImage.setVisibility(View.VISIBLE);
+
+        Animation animation
+                = AnimationUtils.loadAnimation(this, R.anim.scale_animation01);
+        animatedImage.startAnimation(animation);
     }
 }
